072ce4cc6b88b1e3efdeedfb9dfc66c2a858523f
humansandbag <122249555+humansandbag@users.noreply.github.com>
2023-04-01 17:13:45 -0500

    Code cleanup and server-side config syncing
    -Removed unused code
    -Added server-side config syncing
    -Added NetworkCompatability requirements
    -Ran a code cleanup

--- ArmorModifier/ArmorModifier.cs
+++ ArmorModifier/ArmorModifier.cs
@@ -1,52 +1,50 @@
 using BepInEx;
-using Jotunn.Entities;
-using Jotunn.Managers;
-using Jotunn.Utils;
-using HarmonyLib;
 using BepInEx.Configuration;
-using System.Text.RegularExpressions;
-using UnityEngine;
-using System.Collections.Generic;
+using HarmonyLib;
+using Jotunn.Utils;
 using System.Reflection;
+using UnityEngine;
 
 namespace ArmorModifier
 {
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
     [BepInDependency(Jotunn.Main.ModGuid)]
-    //[NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
+    [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
     internal class ArmorModifier : BaseUnityPlugin
     {
         public const string PluginGUID = "MainStreetGaming.ArmorModifier";
         public const string PluginName = "ArmorModifier";
-        public const string PluginVersion = "0.0.1";
+        public const string PluginVersion = "1.0.0";
 
         public static ConfigEntry<float> _helmetModifier;
         public static ConfigEntry<float> _chestModifier;
         public static ConfigEntry<float> _legModifier;
         public static ConfigEntry<float> _capeModifier;
-        public static ConfigEntry<bool> _debugEnabled;
+        //public static ConfigEntry<bool> _debugEnabled;
 
         private void Awake()
         {
             // Jotunn comes with its own Logger class to provide a consistent Log style for all mods using it
-            Jotunn.Logger.LogInfo("ArmorModifier by MainStreet Gaming has loaded.");
+            Jotunn.Logger.LogInfo("Thanks for using ArmorModifier by MainStreet Gaming!");
 
             // To learn more about Jotunn's features, go to
             // https://valheim-modding.github.io/Jotunn/tutorials/overview.html
 
-            _helmetModifier = Config.Bind("ArmorModifiers", "HelmetModifier", 0f, "Percentage modifier for helmet armor (negative values decrease armor, positive values increase armor)");
-            _chestModifier = Config.Bind("ArmorModifiers", "ChestModifier", 0f, "Percentage modifier for chest armor (negative values decrease armor, positive values increase armor)");
-            _legModifier = Config.Bind("ArmorModifiers", "LegModifier", 0f, "Percentage modifier for leg armor (negative values decrease armor, positive values increase armor)");
-            _capeModifier = Config.Bind("ArmorModifiers", "CapeModifier", 0f, "Percentage modifier for cape armor (negative values decrease armor, positive values increase armor)");
-            _debugEnabled = Config.Bind("Debug", "DebugEnabled", false, "Enable debug output");
+            CreateConfigValues();
 
-            //Harmony.CreateAndPatchAll(typeof(ArmorModifier));
             Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
         }
 
+        private void CreateConfigValues()
+        {
+            ConfigurationManagerAttributes isAdminOnly = new ConfigurationManagerAttributes { IsAdminOnly = true };
+            //AcceptableValueRange<float> floatRange = new AcceptableValueRange<float>(-100f, 1000f);
 
-
-
+            _helmetModifier = Config.Bind("ArmorModifiers", "HelmetModifier", 0f, new ConfigDescription("Percentage modifier for helmet armor (negative values decrease armor, positive values increase armor)", null, isAdminOnly));
+            _chestModifier = Config.Bind("ArmorModifiers", "ChestModifier", 0f, new ConfigDescription("Percentage modifier for chest armor (negative values decrease armor, positive values increase armor)", null, isAdminOnly));
+            _legModifier = Config.Bind("ArmorModifiers", "LegModifier", 0f, new ConfigDescription("Percentage modifier for leg armor (negative values decrease armor, positive values increase armor)", null, isAdminOnly));
+            _capeModifier = Config.Bind("ArmorModifiers", "CapeModifier", 0f, new ConfigDescription("Percentage modifier for cape armor (negative values decrease armor, positive values increase armor)", null, isAdminOnly));
+        }
 
         [HarmonyPatch(typeof(ItemDrop.ItemData), "GetArmor", new System.Type[] { typeof(int) })]
         public class ArmorValueModifierPatch
@@ -85,52 +83,6 @@ namespace ArmorModifier
                 return false;
             }
         }
-
-        /*[HarmonyPatch(typeof(ItemDrop.ItemData), "GetTooltip")]
-        public static class ArmorTooltipPatch
-        {
-            public static void Postfix(ItemDrop.ItemData __instance, ref int quality, ref string __result)
-            {
-
-                float armor = __instance.m_shared.m_armor + (float)Mathf.Max(0, quality - 1) * __instance.m_shared.m_armorPerLevel;
-                float newArmor = armor;
-
-                if (__instance.m_shared.m_itemType == ItemDrop.ItemData.ItemType.Helmet)
-                {
-                    newArmor += (newArmor * _helmetModifier.Value / 100f);
-                    //__result = __result.Replace("Armor: " + __instance.m_shared.m_armor.ToString(), "Armor: " + newArmor.ToString());
-                    __result = Regex.Replace(__result, "Armor:\\s(\\d+(\\.\\d+)?)", "Armor: " + newArmor.ToString());
-                }
-                else if (__instance.m_shared.m_itemType == ItemDrop.ItemData.ItemType.Chest)
-                {
-                    newArmor += (newArmor * _chestModifier.Value / 100f);
-                    //__result = Regex.Replace(__result, "Armor:\\s(\\d+(\\.\\d+)?)", "Armor: " + newArmor.ToString());
-                    __result = Regex.Replace(__result, @"\n.*<color=orange></color>", newArmor.ToString());
-                    __result += "<color=orange>TESTING!</color>";
-                }
-                else if (__instance.m_shared.m_itemType == ItemDrop.ItemData.ItemType.Legs)
-                {
-                    newArmor += (newArmor * _legModifier.Value / 100f);
-                    __result = Regex.Replace(__result, "Armor:\\s(\\d+(\\.\\d+)?)", "Armor: " + newArmor.ToString());
-                }
-                else if (__instance.m_shared.m_itemType == ItemDrop.ItemData.ItemType.Shoulder)
-                {
-                    newArmor += (newArmor * _capeModifier.Value / 100f);
-                    __result = Regex.Replace(__result, "Armor:\\s(\\d+(\\.\\d+)?)", "Armor: " + newArmor.ToString());
-                }
-            }
-        }*/
-
-        /*[HarmonyPatch(typeof(SE_Stats), "GetDamageModifiersTooltipString")]
-        static class GetDamageModifiersTooltipString_Patch
-        {
-            static void Postfix(ref string __result, List<HitData.DamageModPair> mods)
-            {
-                {
-
-                }
-            }
-        }*/
 
         public static class ModUtils
         {
